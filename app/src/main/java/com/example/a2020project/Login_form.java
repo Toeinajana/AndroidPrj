9b38d85bbc1d5c305301a6d12f5b2cfc696143d3
Nantawan G <toeinajana@hotmail.com>
2020-07-21 18:31:43 +1000

    working on design

--- app/src/main/java/com/example/a2020project/Login_form.java
+++ app/src/main/java/com/example/a2020project/Login_form.java
@@ -1,7 +1,9 @@
 package com.example.a2020project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login_form extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Login_form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
+        getSupportActionBar().setTitle("Login");
+    }
+
+    public void btn_signup(View view) {
+        Intent SignupIntent = new Intent(Login_form.this, Signup_Form.class);
+        startActivity(SignupIntent);
     }
 }
